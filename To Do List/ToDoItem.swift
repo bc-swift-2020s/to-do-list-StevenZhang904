cdab0cebe2dfee1637acfda32fdd39bc98a1e9da
STEVEN ZHANG <zhangbsr@bc.edu>
2020-02-08 17:02:43 -0500

    5.6

---
+++ To Do List/ToDoItem.swift
@@ -0,0 +1,15 @@
+//
+//  ToDoItem.swift
+//  To Do List
+//
+//  Created by 张泽华 on 2020/2/8.
+//  Copyright © 2020 张泽华. All rights reserved.
+//
+
+import Foundation
+struct ToDoItem{
+    var name: String
+    var date: Date
+    var notes: String
+    
+}
